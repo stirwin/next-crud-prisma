d50b514a74bdbbbee2599d84001fdab22d0aa988
stirwin <stirwincastro@gmail.com>
2024-04-17 16:25:41 -0500

    crud desde el backend listo

---
+++ src/app/api/task/[id]/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+
+export async function GET(request, { params }) {
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+  console.log(task);
+
+  return NextResponse.json(task);
+}
+
+export async function PUT(request, { params }) {
+  const data = await request.json();
+
+  const taskUpdated = await prisma.task.update({
+    where: {
+      id: Number(params.id),
+    },
+    data: data,
+  });
+  return NextResponse.json(taskUpdated);
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const taskRemove = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    return NextResponse.json(taskRemove);
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
+}
